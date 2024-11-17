9d8e6505a37124889317c023b962b5409c32e271
MBriese <mindi.briese@gmail.com>
2024-11-16 22:52:46 -0800

    added heading title and card outline

--- jsTriviaGameApp/src/App.jsx
+++ jsTriviaGameApp/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
-
   return (
-<>
-</>
+    <>
+      <h1 className="main-nav h1">Trivial Pursuit Game</h1>
+        <p className="main-nav">Rules: Answer 6 questions in a category to win a game</p>
+        <div className="cardsContainer">
+            <div className="card">
+            <p>
+                Question: Which of the five Great Lakes does not have a border with Canada?
+            </p>
+            </div>
+            <div className="card-back">
+            <p>
+                Answer: Lake Michigan
+            </p>
+            </div>
+        </div>
+    </>
   )
 }
 
---
+++ jsTriviaGameApp/src/Message.jsx
@@ -0,0 +1,9 @@
+function Message() {
+    return (
+        <>
+            Welcome to Trivial Pursuit!
+        </>
+    );
+}
+
+export default Message;
