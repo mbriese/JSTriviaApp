dc03b7f36b333166c2ea5f8b0a21013ceb91ef0b
MBriese <mindi.briese@gmail.com>
2024-11-17 19:29:57 -0800

    added card outline

--- jsTriviaGameApp/src/App.jsx
+++ jsTriviaGameApp/src/App.jsx
@@ -6,13 +6,22 @@ function App() {
 
   return (
     <>
-      <h1 className="main-nav h1">Trivial Pursuit Game</h1>
-        <p className="main-nav">Rules: Answer 6 questions in a category to win a game</p>
-        <div className="cardsContainer">
-            <div className="card">
-            <p>
-                Question: Which of the five Great Lakes does not have a border with Canada?
-            </p>
+      <div className="main-nav">
+        <h1>Trivial Pursuit Game</h1>
+        <p>Rules: Answer 6 questions in a category to win a game</p>
+      </div>
+        <div className="content">
+            <div className="cardsContainer">
+                <div className="card">
+                    <p>
+                        Question: Which of the five Great Lakes does not have a border with Canada?
+                    </p>
+                <p>Lake Huron</p>
+                <p>Lake Erie</p>
+                <p>Lake Superior</p>
+                <p>Lake Ontario</p>
+                <p>Lake Michigan</p>
+
             </div>
             <div className="card-back">
             <p>
@@ -20,6 +29,7 @@ function App() {
             </p>
             </div>
         </div>
+            </div>
     </>
   )
 }
---
+++ jsTriviaGameApp/src/QandAcard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// eslint-disable-next-line no-unused-vars
+function QandAcard(props) {
+    return (
+        <div>
+
+
+        </div>
+    );
+}
+
+export default QandAcard;
