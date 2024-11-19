7bd8dc6592518766afff1712dc73c121e70cbb42
MBriese <mindi.briese@gmail.com>
2024-11-18 22:26:52 -0800

    built card component

--- jsTriviaGameApp/src/App.jsx
+++ jsTriviaGameApp/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import './QandAcard.jsx'
+import QandAcard from "./QandAcard.jsx";
 function App() {
 
   return (
@@ -11,25 +12,33 @@ function App() {
         <p>Rules: Answer 6 questions in a category to win a game</p>
       </div>
         <div className="content">
-            <div className="cardsContainer">
+            <QandAcard />
+{/*            <div className="cardsContainer">
                 <div className="card">
                     <p>
                         Question: Which of the five Great Lakes does not have a border with Canada?
                     </p>
-                <p>Lake Huron</p>
-                <p>Lake Erie</p>
-                <p>Lake Superior</p>
-                <p>Lake Ontario</p>
-                <p>Lake Michigan</p>
+                <p className="badge">Lake Huron</p>
+                <p className="badge">Lake Erie</p>
+                <p className="badge">Lake Superior</p>
+                <p className="badge">Lake Ontario</p>
+                <p className="badge">Lake Michigan</p>
+            <div>
+                <div className="category-button">Prev Question</div>
+                <div className="category-button">Next Question</div>
 
             </div>
-            <div className="card-back">
-            <p>
-                Answer: Lake Michigan
-            </p>
             </div>
+                <div className="card-back">
+                    <p>
+                        Question: Which of the five Great Lakes does not have a border with Canada?
+                    </p>
+                    <p>
+                        Answer: Lake Michigan
+                    </p>
+                </div>
+            </div>*/}
         </div>
-            </div>
     </>
   )
 }
